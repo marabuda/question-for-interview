ff56d3662994650729664fdeb325224f3795361c
marbuda <a8862688626@gmail.com>
2020-06-29 18:07:16 +0800

    basic function

--- question-for-interview/src/components/HelloWorld.vue
+++ question-for-interview/src/components/HelloWorld.vue
@@ -1,94 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div>
+    <h1>
+      {{msg}}
+    </h1>
+    <div class="mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">{{cacheQuestion.Q}}</h2>
+      </div>
+      <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-dirty">
+        <div>
+          <textarea type="text" rows="4" class="mdl-textfield__input"></textarea>
+          <label class="mdl-textfield__label">try it</label>
+        </div>
+      </div>
+      <div class="mdl-card__supporting-text" v-show="cacheQuestion.isShow">{{cacheQuestion.A}}</div>
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" @click="showAnswer">
+        show answer
+      </button>
+    </div>
+    <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" @click="next">
+      next
+    </button>
   </div>
 </template>
 
 <script>
+import json from '../../static/data.json'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Vue test',
+      data:json.data,
+      random:2,
+      cacheQuestion:{
+        Q:'',
+        A:'',
+        isKnown:false,
+        isShow:false
+      },
+      max:5,
+      min:0
+    }
+  },
+  created(){
+    const vm = this
+    vm.random = Math.floor(Math.random() * (vm.max - vm.min) + vm.min);
+    console.log(vm.random)
+    vm.cacheQuestion.Q = vm.data[vm.random].Q
+    vm.cacheQuestion.A = vm.data[vm.random].A
+    vm.cacheQuestion.isKnown = vm.data[vm.random].isKnown
+    vm.cacheQuestion.isShow = vm.data[vm.random].isKnown
+  },
+  methods:{
+    showAnswer(){
+      const vm = this
+      vm.cacheQuestion.isShow = true
+    },
+    next(){
+      const vm =this
+      vm.random = Math.floor(Math.random() * (vm.max - vm.min) + vm.min);
+      console.log(vm.random)
+      vm.cacheQuestion.Q = vm.data[vm.random].Q
+      vm.cacheQuestion.A = vm.data[vm.random].A
+      vm.cacheQuestion.isKnown = vm.data[vm.random].isKnown
+      vm.cacheQuestion.isShow = vm.data[vm.random].isKnown
     }
   }
 }
@@ -96,18 +72,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.mdl-card{
+  margin: 0 auto 2rem auto;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.mdl-card__supporting-text{
+  text-align: left;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.mdl-textfield{
+  margin: auto;
 }
 </style>
--- question-for-interview/src/router/index.js
+++ question-for-interview/src/router/index.js
@@ -1,7 +1,9 @@
+import VueMdl from 'vue-mdl'
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+Vue.use(VueMdl)
 Vue.use(Router)
 
 export default new Router({
